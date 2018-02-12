2f743d2c37f79d89b4c912c01df1d361303880b5
ezueckg <ezueckg@gmail.com>
2018-02-12 13:10:15 -0600

    parser module using JDOM

---
+++ src/Parser.java
@@ -0,0 +1,30 @@
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Parser {
+
+    public static Document parseXML(String file) {
+
+        SAXBuilder builder = new SAXBuilder();
+        File xmlFile = new File(file);
+
+        try{
+            Document document = builder.build(xmlFile);
+            return document;
+
+        } catch (JDOMException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
