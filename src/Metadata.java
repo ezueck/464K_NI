10d268285a2d022179f4ad53f248d7dfcf2d835c
ezueckg <ezueckg@gmail.com>
2018-02-19 12:50:55 -0600

    methods for metadata

---
+++ src/Metadata.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class Metadata {
+
+    /**
+     * Stores a metadata map into a file
+     * @param file: string of the filename to be created
+     * @param map: the map to be stored
+     */
+    public void storeData(String file, HashMap<Integer, String> map) {
+
+    }
+
+    /**
+     * Build a HashMap stored in the filename given by file
+     * @param file: string of the filename with stored map
+     */
+    public HashMap<Integer, String> buildMap(String file) {
+
+        return null;
+    }
+
+}
